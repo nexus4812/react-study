a949ac509afe9d5438682c8fa1cea4bb6e27bb34
nexus4812 <powerbatteryboy@gmail.com>
2020-03-16 02:29:12 +0900

    compact

--- src/App.js
+++ src/App.js
@@ -2,11 +2,7 @@ import React from 'react';
 
 function App() {
   return (
-    <div >
-        <p>
-            Edit <code>src/App.js</code> and save to reload.
-        </p>
-    </div>
+    <h1 >hello world</h1>
   );
 }
 
